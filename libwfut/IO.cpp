1d92f4b5ba3e86f60b152d42ac33d1e0a78abd9d
Simon Goodall <simon@simongoodall.co.uk>
2006-02-12 17:32:30 +0000

    2005-02-12  Simon Goodall  <libwfut@simongoodall.co.uk>
    	* libwfut/IO.cpp: Fix poll function so download failures are reported
    	  as such. Fix queueFile to set DataStruct as private data for the
    	  handle. The low-level file download code is now fairly complete,
    	  however some work is required for better error handling.

--- libwfut/IO.cpp
+++ libwfut/IO.cpp
@@ -1,16 +1,21 @@
 // This file may be redistributed and modified only under the terms of
 // the GNU Lesser General Public License (See COPYING for details).
-// Copyright (C) 2005 Simon Goodall
+// Copyright (C) 2005 - 2006 Simon Goodall
 
 #include "libwfut/IO.h"
 
 namespace WFUT {
 
-static size_t write_data(void *buffer, size_t size, size_t nmemb,void *userp)  {
-  IO::DataStruct *ds = (IO::DataStruct*)userp;
+static const bool debug = false;
+
+// Callback function to write downloaded data to a file.
+static size_t write_data(void *buffer, size_t size, size_t nmemb,void *userp) {
+  assert(userp != NULL);
+  IO::DataStruct *ds = reinterpret_cast<IO::DataStruct*>(userp);
   if (ds->fp == NULL) {
     ds->fp = fopen(ds->filename.c_str(), "wb");
   }
+  // TODO replace this assert with some error handler
   assert(ds->fp != NULL);
   return fwrite(buffer, size, nmemb, ds->fp);
 }
@@ -20,6 +25,7 @@ int IO::init() {
   curl_global_init(CURL_GLOBAL_ALL);
   m_mhandle = curl_multi_init();
   m_initialised = true;
+  return 0;
 }
 
 int IO::shutdown(){
@@ -44,11 +50,14 @@ int IO::shutdown(){
 
   curl_global_cleanup();
 
-  m_initialised = false;;
+  m_initialised = false;
+
+  return 0;
 }
 
 int IO::queueFile(const std::string &filename, const std::string &url) {
   if (m_files.find(url) != m_files.end()) {
+    fprintf(stderr, "Error file is alreay in queue\n");
     // Url already in queue
     return 1;
   }
@@ -63,6 +72,7 @@ int IO::queueFile(const std::string &filename, const std::string &url) {
   curl_easy_setopt(ds->handle, CURLOPT_URL, url.c_str());
   curl_easy_setopt(ds->handle, CURLOPT_WRITEFUNCTION, write_data);
   curl_easy_setopt(ds->handle, CURLOPT_WRITEDATA, ds);
+  curl_easy_setopt(ds->handle, CURLOPT_PRIVATE, ds);
   curl_multi_add_handle(m_mhandle, ds->handle);
 
   return 0;
@@ -76,32 +86,46 @@ int IO::poll() {
   int msgs;
 
   while ((msg = curl_multi_info_read(m_mhandle, &msgs)) != NULL) {
+
     DataStruct *ds = NULL;
-    if (curl_easy_getinfo(msg->easy_handle, CURLINFO_PRIVATE, (char*)ds) != CURLE_OK) {
+    int err = curl_easy_getinfo(msg->easy_handle, CURLINFO_PRIVATE, &ds);
+    if (err  != CURLE_OK) {
       // Do something on error
+      fprintf(stderr, "Got some error on curl_easy_getinfo (%d)\n", err);
     }
+
+    bool failed = true; 
+    std::string errormsg = "Unknown failure";
     switch (msg->msg) {
       case CURLMSG_DONE: {
-        // close file etc..
-        if (ds) {
-          fclose(ds->fp);
-          ds->fp = NULL;
-          DownloadComplete.emit(ds->url, ds->filename);
-          delete ds;
+        if (msg->data.result == CURLE_OK) {
+          failed = false;
+        } else {
+          failed = true;
+          errormsg = "There was an error downloading the requested file";
         }
-        // Close handle	
-        curl_multi_remove_handle(m_mhandle, msg->easy_handle);
         break;
-        }
+      }
       default:
-        //unhandled case
-	if (ds) {
-          fclose(ds->fp);
-	  DownloadFailed.emit(ds->url, ds->filename);
-          delete ds;
-        }
-        curl_multi_remove_handle(m_mhandle, msg->easy_handle);
+        failed = true;
+        errormsg = "There was an unknown error downloading the requested file";
     }
+    if (ds) {
+      if (ds->fp) fclose(ds->fp);
+      ds->fp = NULL;
+      if (failed) {
+        if (debug) printf("Download Failed\n");
+        DownloadFailed.emit(ds->url, ds->filename);
+      } else {
+        if (debug) printf("Download Complete\n");
+        DownloadComplete.emit(ds->url, ds->filename);
+      }
+      m_files.erase(m_files.find(ds->url));
+      delete ds;
+    }
+    if (debug) printf("Removing Handle\n");
+    // Close handle	
+    curl_multi_remove_handle(m_mhandle, msg->easy_handle);
   }
 
   return num_handles;
