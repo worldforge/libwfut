75b78f285e0b9ca35e4793434342fd542b3a89de
Simon Goodall <simon@simongoodall.co.uk>
2007-11-27 20:11:11 +0000

    2007-11-27  Simon Goodall  <simon@worldforge.org>
    	* libwfut/ChannelFileList.h: Make getFile return a const ref. Added
    	  some comments for doxygen.

--- libwfut/ChannelFileList.h
+++ libwfut/ChannelFileList.h
@@ -10,23 +10,52 @@
 #include <libwfut/types.h>
 
 namespace WFUT {
-
+/**
+ * The ChannelFileList class represents a WFUT channel. It maintainns a list
+ * of FileObjects representing a local or remote directory of files.
+ */ 
 class ChannelFileList {
 public:
   ChannelFileList() {}
   virtual ~ChannelFileList() {}
 
+  /**
+   * Returns the name of the channel.
+   */
   std::string getName() const { return m_name; }
+
+  /**
+   * Set the name of the channel.
+   */
   void setName(const std::string &name) { m_name = name; }
 
-  FileMap getFiles() const { return m_fileMap; }
+  /**
+   * Get the FileMap object storing all of the FileObjects for this channel. 
+   */
+
+  const FileMap &getFiles() const { return m_fileMap; }
+
+  /**
+   * Adds a FileObject to this channel.
+   */
   void addFile(const FileObject &fo) { m_fileMap[fo.filename] = fo ; }
+
+  /**
+   * Removes a FileObject from this channel by it's filename.
+   */ 
   void removeFile(const std::string &filename) {
     FileMap::iterator I = m_fileMap.find(filename);
     if (I != m_fileMap.end()) m_fileMap.erase(I);
   }
 
+  /**
+   * Clear all the files out of the channel.
+   */ 
   void clear() { m_fileMap.clear(); }
+
+  /**
+   * Returns the number of files in the channel.
+   */
   size_t size() const { return m_fileMap.size(); }
 
 private:
