2fe847898fc44dff8e9aabe1c7789ded11f87a1a
Simon Goodall <simon@simongoodall.co.uk>
2007-11-27 20:11:54 +0000

    2007-11-27  Simon Goodall  <simon@worldforge.org>
    	* libwfut/Encoder.h, libwfut/IO.h: Added some doxygen comments.

--- libwfut/Encoder.h
+++ libwfut/Encoder.h
@@ -9,12 +9,27 @@
 
 namespace WFUT {
 
+/**
+ * The Encoder class provides a few utility functions to convert strings to a 
+ * format suitable for storing in an XML document and as a URL.
+ */ 
 class Encoder {
 public:
-  // Encode/Decode string values to store in an XML file
+  /**
+   * Encode the input string so that is can safely be stored in an XML/HTML 
+   * document.
+   */
   static std::string encodeString(const std::string &str);
+
+  /**
+   * Dencode the input string from an XML encoded string back to it's original
+   * form.
+   */
   static std::string decodeString(const std::string &str);
-  // Encode strings so they can be used in URLs
+
+  /**
+   * Encodes characters in the input string so that it is a valid URL string.
+   */
   static std::string encodeURL(const std::string &str);
 };
 
--- libwfut/IO.h
+++ libwfut/IO.h
@@ -29,7 +29,10 @@ namespace WFUT {
     CURL *handle;
   } DataStruct;
 
-
+/**
+ * The IO class wraps most of the CURL related calls, taking a URL to a file and
+ * writing it to a local file.
+ */ 
 class IO {
 public:
   IO() :
@@ -42,20 +45,71 @@ public:
     assert(m_initialised == false);
   }
 
+  /**
+   * The init method initialises the CURL backend. 
+   */
   int init();
+
+  /**
+   * The init method cleans up the CURL backend. 
+   */
   int shutdown();
- 
+
+  /**
+   * Poll network for data chunks to download and write to disk.
+   */ 
   int poll();
+
+  /**
+   * Tell CURL to immediately download the file in the given URL and save it to
+   * the given filename, optionally checking the CRC32 value. This is a blocking
+   *  call.
+   *  @param filename Destination filename
+   *  @param url Remote location.
+   *  @param expected_crc32 The expected crc32 value of the file. 0 to ignore.
+   */
   int downloadFile(const std::string &filename, const std::string &url, uLong expected_crc32);
+
+  /**
+   * Tell CURL to immediately download the file in the given URL and save it 
+   * using the given FILE pointer, optionally checking the CRC32 value. This is
+   * a blocking  call.
+   *  @param fp Open file descriptor to write file data into.
+   *  @param url Remote location.
+   *  @param expected_crc32 The expected crc32 value of the file. 0 to ignore.
+   */
   int downloadFile(FILE *fp, const std::string &url, uLong expected_crc32);
+
+  /**
+   * Queue a file to be downloaded using the poll function.
+   * @see IO::poll
+   * @param path The directory to save the file into.
+   * @param filename The filename minus the directory.
+   * @param url The full URL of the file
+   * @param expected_crc32 The expected CRC32 value of the file. 0 to ignore.
+   * @param executable Flag for whether to set the executable flag when ndownloaded.
+   */ 
   int queueFile(const std::string &path, const std::string &filename, const std::string &url, uLong expected_crc32, bool executable);
 
-  // void yadda(url, filename)
+  /**
+   * The DownloadComplete signal is fired when a file is successfully downloade.
+   */
   sigc::signal<void, const std::string&, const std::string&> DownloadComplete;
-  // void yadda(url, filename, reason)
+  /**
+   * The DownloadFailed signal is fired when a file fails to download 
+   * successfully.
+   */
   sigc::signal<void, const std::string&, const std::string&, const std::string&> DownloadFailed;
 
+  /**
+   * Returns the maximum number of simultaneous downloads allowed.
+   */
   int getMaxDownloads() const { return m_num_to_process; }
+  /**
+   * Set the maximum number of simultaneous downloads allowed. Multiple 
+   * downloads will cause multiple connections to be opened, even if the same
+   * remote server is used.
+   */ 
   void setMaxDownloads(int i) { m_num_to_process = i; }
 
 private:
