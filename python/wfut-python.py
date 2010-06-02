09d058f7c456ef6a87409c7edded9d2c0c540288
Simon Goodall <simon@worldforge.org>
2010-06-02 07:24:12 +0100

    Doc

--- python/wfut-python.py
+++ python/wfut-python.py
@@ -2,20 +2,25 @@
 import os, sys
 from ConfigParser import ConfigParser
 from libwfut import WFUT
-import WFUTCore
+from libwfut import WFUTCore
 
 ## Read global/local config files
 def loadConfig():
+	## Construct filename for user config file
 	filename = os.path.join(os.environ['HOME'], '.libwfutrc');
 
 	config = ConfigParser();
+
+	## TODO: Move into a setDefaults() method?
+
 	## Set some default values
 	config.add_section("general");
-	
 	config.set("general", "server_root", "http://white.worldforgedev.org/WFUT/");
-	
+
+	## Read user config file.	
 	config.read([filename]);
 
+	## TODO: We do not need to do this all the time...
 	## Save config file
 	fp = open(filename, "w");
 	config.write(fp);
@@ -23,7 +28,16 @@ def loadConfig():
 
 	return config;
 
+##def printUsage(invoked):
+##	print invoked." -u channel_name OPTIONS";
+##	print "-u channel name\t (Required) May be '.' to determine from current dir";
+##	print "-s system_path\t Path containing system/read-only channel data"
+##	print "-S server_root\t Url to remote server root";
+##	print "-p local_path\t Path to local updates dir. Current dir is used by default.";
+
+## Load user config
 config = loadConfig();
+
 ## Setup default values
 server_root = config.get("general", "server_root");
 channel = ".";
