1d032418b3e499677418a74ada439d4fc5afb539
Peter Szücs <peter.szucs.dev@gmail.com>
2011-04-29 23:07:45 +0200

    Add temp file support for win32.

--- libwfut/platform.cpp
+++ libwfut/platform.cpp
@@ -24,31 +24,24 @@
 #include <direct.h>
 #define mkdir(path,mode) _mkdir(path)
 #define stat _stat
-// This is a bit of a nasty hack to make sure that when we
-// close a tmp file, we also delete it.
-namespace WFUT {
-typedef std::map<FILE *, char *> TmpFileMap;
-static TmpFileMap tmp_file_map;
-}
 #endif
 
 namespace WFUT {
 
 FILE *os_create_tmpfile() {
 #ifdef _WIN32
-  // Check to see if there are temp dirs defined, else use current directory.
-  static const char cwd[] = ".";
-  const char *tmpdir = getenv("TMP");
-  const char *tempdir = getenv("TEMP");
-  // Make the choice
-  const char *t_dir = (tmpdir != 0) ? (tmpdir) : ( (tempdir != 0) ? (tempdir) : (cwd)); 
-  char *filename = tempnam(t_dir, "wfut");
-  FILE *fp = fopen(filename, "w+b");
-  if (fp != NULL) {
-    // Record the filename for the FILE pointer
-    tmp_file_map[fp] = filename;
-  }
-  return fp;
+  char path[MAX_PATH];
+  int ret = GetTempPathA(MAX_PATH, path);
+  if(ret > MAX_PATH || (ret == 0))
+    strcpy(path, ".\\");
+  char filename[MAX_PATH];
+  //only 3 prefix characters allowed
+  ret = GetTempFileNameA(path,"wfu",0,filename);
+  if(ret == 0)
+    sprintf(filename,"%swfut%d.tmp",path,rand());
+  //There is a special 'D' sign, which means temporary and delete on close 
+  //http://msdn.microsoft.com/en-us/library/yeby3zcb%28v=vs.71%29.aspx
+  return fopen(filename, "w+bD");
 #else
   return tmpfile();
 #endif
@@ -56,17 +49,8 @@ FILE *os_create_tmpfile() {
 
 void os_free_tmpfile(FILE *fp) {
   assert(fp != 0);
-#ifdef _WIN32
-  TmpFileMap::iterator I = tmp_file_map.find(fp);
-  assert (I != tmp_file_map.end());
-  fclose(I->first);       // Close file handle
-  unlink(I->second);      // Delete the tmp file
-  free(I->second);        // Free up the mem allocated to the filename
-  tmp_file_map.erase(I);  // Get rid of the record.
-#else 
- // tmpfile cleans up after itself
+  // tmpfile cleans up after itself
   fclose(fp);
-#endif
 }
 
 int os_mkdir(const std::string &dir) {
